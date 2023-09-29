c0158cf8b8f3afc045ef47b13e140f64adeee40d
RenanBeraldi <renanberaldi@outlook.com>
2023-09-29 10:53:52 -0300

    preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 "use strict";
 
+// PRELOAD
+// loading will be end after document is loaded
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
